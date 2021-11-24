2b95c340d1edf40e4ed00c2f90fcc624bad20a0d
jonathan <jonathan.mendiola@softhealth.com.mx>
2021-11-23 20:20:22 -0600

    cambios para cerrar

--- src/components/Cierre.jsx
+++ src/components/Cierre.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
 export const Cierre = (props) => {
+    const cerrar = () => {
+        window.open("about:blank", "_self");
+        window.close();
+    }
     return (
         <div className="contenedor">
             <img className="fondo-imagen" src={'/assets/Fondo_encuesta.png'} alt="fondo" />
             <div className="contenedor-imagen">
                 <img src='assets/Modal_cierre.png' alt="Cierre" className="Modal-imagen" />
-                <img onClick={props.nextStep} src='/assets/boton_cierre.png' alt="Cierre" className="Modal-boton Modal-boton-derecha" />
+                <img onClick={() => cerrar()} src='/assets/boton_cierre.png' alt="Cierre" className="Modal-boton Modal-boton-derecha" />
             </div>
         </div>
     )
